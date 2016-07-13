e8c9c2686b05c589f0115e875d4cfbd5af776e3e
Pete Birley <petebirley@gmail.com>
2016-07-13 03:36:16 +0100

    Update run-ipam.sh

--- vagrant_legacy/run-ipam.sh
+++ vagrant_legacy/run-ipam.sh
@@ -4,7 +4,6 @@ docker run -d \
 --name ipam \
 -p ${OVN_IP}:5000:5000 \
 -p ${OVN_IP}:9696:9696 \
--p ${OVN_IP}:6640:6640 \
 -p ${OVN_IP}:6641:6641 \
 -p ${OVN_IP}:6642:6642 \
 docker.io/port/ovn-ipam:latest /start.sh
