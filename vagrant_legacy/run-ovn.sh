a51d2273b839f0329caa09aa48edf061c9a7494f
Pete Birley <petebirley@gmail.com>
2016-07-13 03:23:33 +0100

    update ovn startup script

--- vagrant_legacy/run-ovn.sh
+++ vagrant_legacy/run-ovn.sh
@@ -1,9 +1,12 @@
 IPAM_IP=$1 # 192.168.50.101
-LOCAL_IP=$2 #192.168.50.101
+LOCAL_OVS_IP=$2 #192.168.50.101
 
 
-ovn-integrate create-integration-bridge
-ovn-integrate set-ipam $IPAM_IP
-ovn-integrate set-tep $LOCAL_IP
-ovn-container init --bridge br-int --overlay-mode
-ovn-controller --pidfile --detach -vconsole:off --log-file
+#LOCAL_OVS_IP=$(ip -f inet -o addr show eth1|cut -d\  -f 7 | cut -d/ -f 1)
+ovs-vsctl set Open_vSwitch . external_ids:ovn-remote="tcp:$IPAM_IP:6642"
+ovs-vsctl set Open_vSwitch . external_ids:ovn-nb="tcp:$IPAM_IP:6641"
+ovs-vsctl set Open_vSwitch . external_ids:ovn-encap-ip="$LOCAL_OVS_IP"
+ovs-vsctl set Open_vSwitch . external_ids:ovn-encap-type="geneve"
+
+
+/usr/share/openvswitch/scripts/ovn-ctl start_controller
