8389dcba66e9f5bd96d04461ae8ad36ec6214bf4
Pete Birley <petebirley@gmail.com>
2016-07-13 03:33:42 +0100

    Update run-ipam.sh

--- vagrant_legacy/run-ipam.sh
+++ vagrant_legacy/run-ipam.sh
@@ -2,6 +2,7 @@
 OVN_IP=$(ip -f inet -o addr show eth1|cut -d\  -f 7 | cut -d/ -f 1)
 docker run -d \
 --name ipam \
+-p ${OVN_IP}:5000:5000 \
 -p ${OVN_IP}:9696:9696 \
 -p ${OVN_IP}:6640:6640 \
 -p ${OVN_IP}:6641:6641 \
